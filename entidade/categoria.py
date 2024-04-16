40618ce02349cf17fffdf4775b2984e9690ea24e
mariana <marianaamaralsteffen@gmail.com>
2024-04-16 20:11:31 -0300

    Tentando fazer a tela

--- entidade/categoria.py
+++ entidade/categoria.py
@@ -1,11 +1,14 @@
 class tipoRestauracao:
-    LAVAR = 'Lavagem'
-    PASSAR = 'Passar'
-    REPARO = 'Reparo de danos'
-    DETALHES = 'Restauração de detalhes'
-    REMOCAO = 'Remocão de manchas'
-    TINGIMENTO = 'Tingimento'
-    CUSTOMIZACAO = 'Customização'
+
+    tipos = {
+        "LAVAR": 'Lavagem',
+        "PASSAR": 'Passar',
+        "REPARO": 'Reparo de danos',
+        "DETALHES": 'Restauração de detalhes',
+        "REMOCAO": 'Remocão de manchas',
+        "TINGIMENTO": 'Tingimento',
+        "CUSTOMIZACAO": 'Customização'
+    }
 
 class Categoria:
     def __init__(self, id, tipo, custo: float):
--- limite/tela_peca.py
+++ limite/tela_peca.py
@@ -1,48 +1,66 @@
 import tkinter as tk
-
-
-class Tela:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Cadastro")
-
-    def gerar_label(self, texto):
-        frame = tk.Frame(self.master)
-        label = tk.Label(frame, text=texto)
-        entry = tk.Entry(frame)
-        frame.pack()
-        label.pack(side=tk.LEFT)
-        entry.pack(side=tk.LEFT)
-        return entry
-
-    def gerar_botao(self, texto, command=None):
-        return tk.Button(self.master, text=texto, command=command)
+from limite.tela import Tela
+from entidade.categoria import tipoRestauracao as tr
 
 
 class TelaPeca(Tela):
     def __init__(self, master):
         super().__init__(master)
-        self.master.title("Cadastro de peça")
+        self.master.title("Cadastro de peças")
 
     def pegar_dados(self):
-        entry_nome = super().gerar_label("Nome:")
-        entry_email = super().gerar_label("Email:")
-        botao_cadastrar = super().gerar_botao("Cadastrar", command=self.cadastrar)
-        botao_cadastrar.pack()
-        return entry_nome, entry_email
+        # Campos de texto
+        entry_nome = super().gerar_input("Título:")
+        # Botão de escolher imagem
+        imagem = super().gerar_botao(texto="Escolher imagem",
+                                     command=super().abrir_imagem)
+        # Imprimir a imagem
+        if imagem:
+            super().exibir_imagem(imagem)
 
-    def cadastrar(self):
+        # Para selecionar o Status
+        status_frame = tk.Frame(self.master)
+        status_frame.pack()
+        status_label = tk.Label(status_frame, text="Status")
+        status_label.pack(side=tk.LEFT)
+        opcoes = ["Em restauração", "À venda"]
+        status = super().gerar_radiobutton(opcoes, status_frame)
 
+        # Criando um dicionário com os resultados
+        dados = {"titulo": entry_nome.get(),
+                 "imagem": imagem,
+                 "status": status.get()}
+
+        # Botão para concluir
+        super().gerar_botao(texto="Continuar",
+                            command=lambda: self.tratar_dados(dados))
+
+    def tratar_dados(self, dados):
+        # Renderizando atributos de acordo com o tipo de Status
+        if dados["status"] == "Em restauração":
+            # Selecionando o tipo de restauração
+            tipo_frame = tk.Frame(self.master)
+            tipo_frame.pack()
+            tipo_label = tk.Label(status_frame, text="Selecione as restaurações\n necessárias:")
+            tipo_label.pack(side=tk.LEFT)
+            # Os tipos foram importados
+            opcoes = tr.tipos
+            tipos_selecionados = super().gerar_checkbox(opcoes, tipo_frame)
+        if dados["status"] == "À venda":
+            preco_entry = super().gerar_input("Preço:")
+
+    def iniciar(self):
+        super().gerar_texto_packed("Cadastro de peça")
+        self.pegar_dados()
 
 
 def main():
     root = tk.Tk()
-    root.geometry("400x200")
+    root.geometry("400x500")
 
     tela = TelaPeca(root)
 
-    infos = tela.pegar_dados()
-    print(infos)
+    tela.iniciar()
 
     root.mainloop()
 
