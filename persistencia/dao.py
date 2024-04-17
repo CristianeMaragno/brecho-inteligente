84769a270289fc5b6a8532e82c5a4e7b6f977823
Cristiane R. Maragno <cristiane.maragno@effecti.com.br>
2024-04-17 20:33:20 -0300

    Add database connection

---
+++ persistencia/dao.py
@@ -0,0 +1,37 @@
+import sqlite3
+from abc import ABC
+
+class DAO(ABC):
+    def __init__(self):
+        self.db_name = "brecho.db"
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+
+    def disconnect(self):
+        if self.conn:
+            self.conn.close()
+
+    def create_table(self, table_name, columns):
+        column_definitions = ', '.join([f"{col_name} {col_type}" for col_name, col_type in columns.items()])
+        query = f"CREATE TABLE IF NOT EXISTS {table_name} ({column_definitions})"
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def insert_data(self, table_name, data):
+        placeholders = ', '.join(['?' for _ in range(len(data[0]))])
+        query = f"INSERT INTO {table_name} VALUES ({placeholders})"
+        self.cursor.executemany(query, data)
+        self.conn.commit()
+
+    def fetch_data(self, table_name):
+        query = f"SELECT * FROM {table_name}"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
--- persistencia/usuario_dao.py
+++ persistencia/usuario_dao.py
@@ -1,17 +1,27 @@
+from persistencia.dao import DAO
 from entidade.usuario import Usuario
 
 
-class UsuarioDAO():
+class UsuarioDAO(DAO):
     def __init__(self):
-        self.__database = 0
+        super().__init__()
+        super().connect()
+        super().create_table('users', {'id': 'INTEGER PRIMARY KEY', 'email': 'TEXT', 'senha': 'TEXT', 'papel': 'INTEGER'})
     def add(self, usuario: Usuario):
-        return 0
+        data = [
+            (1, 'john_doe', 'john@example.com'),
+            (2, 'jane_smith', 'jane@example.com')
+        ]
+        super().insert_data('users', data)
 
     def remove(self, codigo: int):
         return 0
 
     def get_all(self):
-        return 0
+        return super().fetch_data('users')
 
     def update(self, codigo, usuario: Usuario):
         return 0
+
+    def execute(self, custom_query):
+        super().execute_query(custom_query)
